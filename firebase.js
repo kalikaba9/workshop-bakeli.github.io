6a6864ed2f861181932afa699b4658596b43851c
Mouhamed Ahmadou Ba <kalikaba9@gmail.com>
2022-11-19 11:11:35 +0000

    first commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDdvnfeetRPlP_MFkWGKGrtP5hWY7sAGV0",
+    authDomain: "fewnu-medical.firebaseapp.com",
+    databaseURL: "https://fewnu-medical-default-rtdb.firebaseio.com",
+    projectId: "fewnu-medical",
+    storageBucket: "fewnu-medical.appspot.com",
+    messagingSenderId: "1016969605905",
+    appId: "1:1016969605905:web:e3102a40524416829652b0",
+    measurementId: "G-BD801M6GQY"
+  };
